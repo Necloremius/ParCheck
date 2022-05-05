5bf9cff7bbf9dd3332b41eadb681fd5c16dadd3d
Amdirpherian <josmo314@gmail.com>
2022-05-05 16:08:51 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use rocket::{Rocket, Build};
 #[get("/check/<num>")]
 fn pch(num: i128) -> &'static str { if num % 2 == 0 { "even" } else { "odd" }} // checks if the number is even or odd, then returns this result
 
-
 #[launch]
 fn rocket() -> Rocket<Build> {
   rocket::build().mount("/check/<num>", routes![pch])
 }
+
+
+
+
+
+
+
+
+
