497abcc8e03ad0a8a626a44a1cd274f4163db829
Amdirpherian <josmo314@gmail.com>
2022-05-07 09:28:18 -0400

    updated stuffs

---
+++ projects/Nautilus/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.btn')
+const form = document.querySelector('.form')
+const username = document.querySelector('')
+button.addEventListener('click', function() {
+   form.classList.add('form--no') 
+   
+   alert("Info sent")
+});
